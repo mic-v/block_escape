3331cd711545f2593da2c8bfdeb70b9c9826a17e
Michael Victorino <michaelvictorino@gmail.com>
2023-02-04 10:05:00 -0500

    Changed editor version of project

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -28,12 +28,12 @@ public class Movement : MonoBehaviour {
     void Look() {
         if(_input != Vector3.zero)
         {
-            var matrix = Matrix4x4.Rotate(Quaternion.Euler(0,-45,0));
+            var matrix = Matrix4x4.Rotate(Quaternion.Euler(0,-20,0));
 
             var skewedInput = matrix.MultiplyPoint3x4(_input);
 
             var relative = (transform.position + skewedInput) - transform.position;
-            var rot = Quaternion.LookRotation(relative);
+            var rot = Quaternion.LookRotation   (relative);
 
             transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, _turnSpeed);
         }
